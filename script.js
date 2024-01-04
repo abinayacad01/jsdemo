560a31a35c9bfe800d20d9cfb72db645cd9bbaa7
abinayacad01 <abinayakumarcad01@gmail.com>
2024-01-04 17:42:25 +0530

    basic codes of js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hi");
+console.log("hey there!");
+console.log(512200411102004);
+//to print object
+console.log({fname:"abinaya",lname:"jayakumar"});
+console.table({fname:"abinaya",lname:"jayakumar"});
+//to print array
+console.log([11,10,2004]);
+console.table([11,10,2004,5,12,2004]);
+//variable declaration
+var a=100;
+var b=200;
+if(a>50)
+{
+    console.log(true);
+}
+else
+{
+    console.log(false);
+}
